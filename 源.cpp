1e49317d08e084cb42836af8bd1dc19db8088f72
泽宇 lee <1819757699@qq.com>
2021-01-19 14:33:31 +0800

    加入表盘

--- 源.cpp
+++ 源.cpp
@@ -16,7 +16,7 @@ int main(void)
 	int secondLength,minuteLength,hourLength;                          
 	secondLength = Width / 5;                 //秒钟的长度
 	minuteLength = Width / 6;                 //分钟长度
-	hourLength = Width / 10;                  //时钟长度
+	hourLength = Width / 8;                  //时钟长度
 
 	int secondEnd_x = 0, secondEnd_y = 0,
 		minuteEnd_x = 0, minuteEnd_y = 0,
@@ -39,7 +39,11 @@ int main(void)
 		minuteAngle = ti.wMinute * 2 * PI / 60;
 		hourAngle = ti.wHour * 2 * PI / 60;
 
-		secondEnd_x = center_x + secondLength * sin(secondAngle);
+		//制作表盘，绘制黄线，白色填充圆
+		setcolor(YELLOW);
+		fillcircle(center_x, center_y, Width/5+5);
+
+		secondEnd_x = center_x + secondLength * sin(secondAngle); //三个指针的变化
 		secondEnd_y = center_y - secondLength * cos(secondAngle);
 		minuteEnd_x = center_x + minuteLength * sin(minuteAngle);
 		minuteEnd_y = center_y - minuteLength * cos(minuteAngle);
@@ -47,7 +51,7 @@ int main(void)
 		hourEnd_y = center_y - hourLength * cos(hourAngle);
 		//画秒钟
 		setlinestyle(PS_SOLID, 2);
-		setcolor(WHITE);
+		setcolor(BLACK);
 		line(center_x, center_y, secondEnd_x, secondEnd_y);
 		setlinestyle(PS_SOLID, 5);
 		setcolor(BLUE);
@@ -58,7 +62,7 @@ int main(void)
 		Sleep(1000);
 		FlushBatchDraw();
 		//隐藏前一帧的秒钟
-		setcolor(BLACK);
+		setcolor(WHITE);
 		line(center_x, center_y, secondEnd_x, secondEnd_y);
 		line(center_x, center_y, minuteEnd_x, minuteEnd_y);
 		line(center_x, center_y, hourEnd_x, hourEnd_y);
